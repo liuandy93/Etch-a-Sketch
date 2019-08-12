5ef7bf7e2a4eca0c68500a9c2de6bb92c696b483
liuandy <liuandy20@gmail.com>
2019-08-12 09:57:26 -0700

    Final Version before Styling

--- eas.js
+++ eas.js
@@ -1,11 +1,8 @@
-    let rgbOn = false;
-    let blackFill = true;
-    let opacityFill = false;
-    let rgbValue;
-    let gridContainer = document.getElementById('grid-container');
-    gridContainer.style.gridTemplateColumns = "repeat(16, 1fr)";
-    gridContainer.style.gridTemplateRows = "repeat(16, 1fr)";
-    let gridSize = 256;
+let rgbValue;
+let gridContainer = document.getElementById('grid-container');
+gridContainer.style.gridTemplateColumns = "repeat(16, 1fr)";
+gridContainer.style.gridTemplateRows = "repeat(16, 1fr)";
+let gridSize = 256;
 
 
 function generateGrid() {
@@ -16,16 +13,12 @@ function generateGrid() {
         let div = document.createElement('div');
         gridContainer.appendChild(div);
         div.classList.add('grid-item');
-        //div.addEventListener("mouseenter", mouseOver);
     }
 }
 
   generateGrid();
 
-
 	$('#incrementOpacity').click(function(){
-    rgbOn = false;
-    blackFill = false;
 		$('.grid-item').css('opacity',0);
 		$('.grid-item').hover(function(){
       $(this).css('background-color', '#000');
@@ -36,22 +29,7 @@ function generateGrid() {
 		});
 	});
     
-
-    var etch = function(){
-        $('.grid-item').on('mouseover', function(){
-          if(rgbOn) {
-            rgbMode();
-            $(this).css("background-color", "#" + rgbValue); 
-          } else if (blackFill) {
-          $(this).css("background-color", "#000");
-          } 
-        });
-      };
-    
-    //etch();
-
     function resetGame() {
-          rgbOn = false;
           $('.grid-item').css("background-color", "#ffffff");
           gridSize = 0;
           gridSize = parseInt(prompt("How large would you like the grid to be? i.e 30x30, 60x60, 100x100"));
@@ -62,26 +40,20 @@ function generateGrid() {
           generateGrid();          
       }
 
-    let rgbMode = () => {
-      //toString(16) will turn the number into a hexadecimal vaue
-      rgbOn = true;
-      blackFill = false;
+    $('#blackMode').click(function() {
+      $('.grid-item').css({"background-color": "#ffffff", 'opacity': 1});
+      $('.grid-item').hover(function() {
+        $(this).css('background-color', '#000');
+      });
+    });
+
+    $('#rgbMode').click(function() {
+      $('.grid-item').css({"background-color": "#ffffff", 'opacity': 1});
+      $('.grid-item').hover(function() {
       rgbValue = Math.floor(Math.random() * 16777215).toString(16);
-    }
-
-    let blackMode = () => {
-      rgbOn = false;
-      opacityFill = false;
-      blackFill = true;
-      etch();
-    }
-
-    let opacityMode = () => {
-      rgbOn = false;
-      blackFill = false;
-      opacityFill = true;
-      etch();
-    }
+      $(this).css('background-color', '#' + rgbValue);
+    });
+  });
 
 
 
